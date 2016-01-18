28deb5713b97d9c1a65fcb62b2875af095643970
Scott Humphries <git@sscotth.io>
2016-01-18 14:42:21 -0600

    Finished Lesson

---
+++ constructed.js
@@ -0,0 +1,10 @@
+function Building () {
+  this.isBurned = false;
+}
+
+Building.prototype.burn = function () {
+  console.log('🔥');
+  this.isBurned = true;
+};
+
+module.exports = new Building();
---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('This is an index file');
+
+x = 'global variable';
+
+var y = 'local variable'
+
+// var exports = module.exports = {};
+
+exports.b = 'c';
+module.exports = {a: 'b'};
+exports.c = 'd';
